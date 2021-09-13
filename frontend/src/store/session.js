402a90f50ae03a0fd0775f390d6c239d38f676dd
Andres <aecheverri43@gmail.com>
2021-09-12 23:31:07 -0700

    Complete heroku setup

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,7 +16,7 @@ export const restoreUser = () => async dispatch => {
     dispatch(setUser(data.user));
     return response;
   };
-  
+
 const removeUser = () => {
   return {
     type: REMOVE_USER,
@@ -37,6 +37,28 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
